81214dc1aea3c326ca0e874a2d8c5855f82c998c
lessan-cyber <121871770+lessan-cyber@users.noreply.github.com>
2023-12-21 18:42:52 +0000

    building the first feature on first_feature branch

---
+++ exo2.py
@@ -0,0 +1,6 @@
+liste =["aziz", "lessan", "mo",1,"mariam",69,"wiskid", "burna Boy"]
+for element in liste:
+    if type(element) == str:
+        print(element)
+    else:
+        print("not a string")
