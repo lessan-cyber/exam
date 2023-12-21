df261c07cdb5b46b35d956a31b40e3ba98d3e960
lessan-cyber <121871770+lessan-cyber@users.noreply.github.com>
2023-12-21 19:06:34 +0000

    add file call dictionnaire.py in structure_de_donnee folder

---
+++ structure_de_donnee/dictionnaire.py
@@ -0,0 +1,10 @@
+dictionnaire = {
+    "cle1": "valeur1",
+    "cle2": "valeur2",
+    "cle3": "valeur3",
+    "cle4": "valeur4",
+    "cle5": "valeur5",
+}
+
+# Afficher le dictionnaire
+print(dictionnaire)
